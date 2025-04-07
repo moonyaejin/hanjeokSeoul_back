cd4bc971ecad43ae8eedb9cd998404c55a5768df
juytj21 <juytj21@gmail.com>
2025-04-07 15:04:41 +0900

    초기 구성

---
+++ src/main/java/com/hanjeokseoul/quietseoul/QuietseoulApplication.java
@@ -0,0 +1,13 @@
+package com.hanjeokseoul.quietseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuietseoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuietseoulApplication.class, args);
+	}
+
+}
