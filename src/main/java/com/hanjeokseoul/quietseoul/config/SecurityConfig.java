a5d126e8d77942e551d945f544378c949c4bfd85
Seojin <juytj21@gmail.com>
2025-04-15 21:46:09 +0900

    로그인 차단 허용

--- src/main/java/com/hanjeokseoul/quietseoul/config/SecurityConfig.java
+++ src/main/java/com/hanjeokseoul/quietseoul/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())) // ✅ 추가
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -48,6 +49,7 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(
