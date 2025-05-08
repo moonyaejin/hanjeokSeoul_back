c7b1f681fce4352d421f169c7167ef72b7c95ac6
moonyaejin <i0209i80@gmail.com>
2025-05-08 10:56:10 +0900

    webconfig 수정

--- src/main/java/com/hanjeokseoul/quietseoul/config/WebConfig.java
+++ src/main/java/com/hanjeokseoul/quietseoul/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 배포 시에는 실제 프론트 도메인으로 바꿔야 함!
+                .allowedOrigins("https://back-4bpd.onrender.com")
                 .allowedMethods("*");
     }
 }
